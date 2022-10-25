8ab67c03ff2f9ba6508c23c59f32eff246ebd326
Dysprosium <dysprosium@outlook.com.br>
2022-10-24 21:36:19 -0300

    GERPESSAPI24102022213025FEATURE
    - Configuração Inicial do Projeto

---
+++ src/main/java/digital/quintino/gerpessapi/GerpessapiApplication.java
@@ -0,0 +1,13 @@
+package digital.quintino.gerpessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerpessapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerpessapiApplication.class, args);
+	}
+
+}
